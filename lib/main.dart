9480208b796331d49b0efb7df596710698c6c41e
stevendz <steven.dzionara@gmail.com>
2020-09-08 09:27:22 +0200

    Add codemagic.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Parking',
       theme: ThemeData(),
       home: HomePage(),
