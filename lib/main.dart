c3bef3bb5099b0e606d7ff4313b419db5ee6df32
stevendz <steven.dzionara@gmail.com>
2020-09-08 14:04:17 +0200

    Add and display dummy markers

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,65 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    List<Marker> markers = [
+      Marker(
+          markerId: MarkerId('slot1'),
+          position: LatLng(53.5644297, 10.0502048),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('slot1'),
+                // actions: [
+                //   IconButton(
+                //     icon: Icon(Icons.bookmark_border),
+                //     onPressed: () {},
+                //   ),
+                //   IconButton(
+                //     icon: Icon(Icons.beenhere),
+                //     onPressed: () {},
+                //   ),
+                // ],
+              ),
+            );
+          }),
+      Marker(
+          markerId: MarkerId('slot2'),
+          position: LatLng(53.5694297, 10.0602048),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('slot2'),
+              ),
+            );
+          }),
+      Marker(
+          markerId: MarkerId('slot3'),
+          position: LatLng(53.5584297, 10.0552048),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('slot3'),
+              ),
+            );
+          }),
+      Marker(
+          markerId: MarkerId('slot4'),
+          position: LatLng(53.5554297, 10.0352048),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('slot4'),
+              ),
+            );
+          }),
+    ];
     return Container(
       child: GoogleMap(
+        markers: Set.from(markers),
         initialCameraPosition: CameraPosition(
           target: LatLng(53.5644297, 10.0502048),
           zoom: 14,
