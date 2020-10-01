ff664fd97b56aaf207a2717f3e1934b8253b49a6
stevendz <steven.dzionara@gmail.com>
2020-10-01 11:57:18 +0200

    Create EditSlotScreen and add swipe to edit on MySlotsScreen

--- lib/screens/my_slots_screen.dart
+++ lib/screens/my_slots_screen.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:parking/screens/edit_slot_screen.dart';
+import 'package:parking/screens/profile_screen.dart';
 
 class MySlotsScreen extends StatefulWidget {
   @override
@@ -43,21 +47,57 @@ class _MySlotsScreenState extends State<MySlotsScreen> {
                     itemBuilder: (context, index) {
                       return Column(
                         children: <Widget>[
-                          ListTile(
-                            leading: Container(
-                              width: 50,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                      data[index].data()['imageUrl'],
+                          Dismissible(
+                            key: Key(data[index].id),
+                            onDismissed: (direction) {
+                              if (direction == DismissDirection.startToEnd) {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => EditSlotScreen(
+                                      slot: data[index],
                                     ),
-                                    fit: BoxFit.cover),
-                              ),
+                                  ),
+                                );
+                              }
+                              if (direction == DismissDirection.endToStart) {
+                                print(data[index].data());
+                                // slotsDb.doc(data[index].id).delete();
+                              }
+                            },
+                            background: Container(
+                              color: Theme.of(context).primaryColorLight,
+                              alignment: Alignment.centerLeft,
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Icon(Icons.edit),
                             ),
-                            title: Text(data[index].data()['title']),
+                            secondaryBackground: Container(
+                              color: Colors.redAccent,
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Icon(Icons.delete_outline,
+                                  color: Colors.white),
+                            ),
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Container(
+                                    width: 50,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                            data[index].data()['imageUrl'],
+                                          ),
+                                          fit: BoxFit.cover),
+                                    ),
+                                  ),
+                                  title: Text(data[index].data()['title']),
+                                ),
+                                Divider()
+                              ],
+                            ),
                           ),
-                          Divider()
                         ],
                       );
                     },
